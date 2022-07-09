303ba1147600bb01d359dbbf63d0521b9935e56a
Xinpeng Wang <xinpeng.wang@tum.de>
2022-07-10 00:48:20 +0200

    fix fp32 issue for kl_div

--- methods/feature_distill.py
+++ methods/feature_distill.py
@@ -14,16 +14,16 @@ def att_val_kl(student_atts, student_vals, teacher_atts, teacher_vals, layer_sel
 
     for student_att, teacher_att in zip(student_atts, new_teacher_atts):
         # batch_size, head_num, lenght = student_att.shape[0], student_att.shape[1], student_att.shape[2]
-        loss_kl_tmp = F.kl_div(torch.log(student_att), teacher_att, reduction='sum')/ (batch_size * num_head * length) #, reduction='batchmean', log_target=True)
+        loss_kl_tmp = F.kl_div(torch.log(student_att.float()), teacher_att.float(), reduction='sum')/ (batch_size * num_head * length) #, reduction='batchmean', log_target=True)
         loss_att += loss_kl_tmp
 
     new_teacher_value = [teacher_vals[i] for i in layer_selection]
 
     for student_value, teacher_value in zip(student_vals, new_teacher_value):
-        vr_student = F.log_softmax(torch.bmm(student_value, student_value.transpose(1,2))/dk_sqrt, dim=-1)
-        vr_teacher = F.softmax(torch.bmm(teacher_value, teacher_value.transpose(1,2))/dk_sqrt, dim=-1)
+        vr_student = F.log_softmax(torch.bmm(student_value.reshape(-1, length, dk), student_value.reshape(-1, length, dk).transpose(1,2))/dk_sqrt, dim=-1)
+        vr_teacher = F.softmax(torch.bmm(teacher_value.reshape(-1, length, dk), teacher_value.reshape(-1, length, dk).transpose(1,2))/dk_sqrt, dim=-1)
 
-        loss_value_tmp = F.kl_div(vr_student, vr_teacher, reduction='sum')/(batch_size * num_head * length)
+        loss_value_tmp = F.kl_div(vr_student.float(), vr_teacher.float(), reduction='sum')/(batch_size * num_head * length)
         loss_value += loss_value_tmp
     # loss  = loss_att + loss_value
-    return loss_att, loss_value
+    return loss_att.half(), loss_value.half()
