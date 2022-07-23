5a5fd6f3bcf6d895e73ea29963a544c38b585267
Xinpeng Wang <xinpeng.wang@tum.de>
2022-07-23 22:17:24 +0200

    add two stage frame

--- methods/feature_distill.py
+++ methods/feature_distill.py
@@ -1,6 +1,8 @@
 import torch.nn.functional as F
 import math
 import torch
+from pretraining.utils import master_process
+# import wandb
 
 def att_val_kl(student_atts, student_vals, teacher_atts, teacher_vals, layer_selection):
     #TODO: 把这个fp16 32， 正规化，以及看amp方案
@@ -13,7 +15,7 @@ def att_val_kl(student_atts, student_vals, teacher_atts, teacher_vals, layer_sel
     layer_selection = [int(item) for item in layer_selection.split(',')]
 
     new_teacher_atts = [teacher_atts[i] for i in layer_selection]
-
+    student_atts = [student_atts[-1]]
     #TODO: change to softmax and log 
     for student_att, teacher_att in zip(student_atts, new_teacher_atts):
         # batch_size, head_num, lenght = student_att.shape[0], student_att.shape[1], student_att.shape[2]
@@ -25,6 +27,7 @@ def att_val_kl(student_atts, student_vals, teacher_atts, teacher_vals, layer_sel
 
     new_teacher_value = [teacher_vals[i] for i in layer_selection]
 
+    student_vals = [student_vals[-1]]
     for student_value, teacher_value in zip(student_vals, new_teacher_value):
         vr_student = F.log_softmax(torch.bmm(student_value.reshape(-1, length, dk), student_value.reshape(-1, length, dk).transpose(1,2))/dk_sqrt, dim=-1)
         vr_teacher = F.softmax(torch.bmm(teacher_value.reshape(-1, length, dk), teacher_value.reshape(-1, length, dk).transpose(1,2))/dk_sqrt, dim=-1)
@@ -34,3 +37,40 @@ def att_val_kl(student_atts, student_vals, teacher_atts, teacher_vals, layer_sel
         loss_value += loss_value_tmp
     # loss  = loss_att + loss_value
     return loss_att, loss_value
+
+
+def att_val_frame(teacher, student, args, batch):
+    with torch.no_grad():
+        attentions_teacher, values_teacher, prediction_score_teacher = \
+                teacher(batch, output_attentions=True, output_values=True, output_loss=False)
+    mlm_loss , attentions_st, values_st, prediction_score_st = \
+            student.forward(batch, output_attentions=True, output_values=True, output_loss=True)
+
+    loss_att, loss_val = \
+            att_val_kl(attentions_st, values_st, attentions_teacher, values_teacher, args.layer_selection)
+    
+    return loss_att, loss_val
+
+def twostage(teacher, student,args, batch, time_diff, global_step, wandb):
+    time_proportion = time_diff / args.total_training_time
+    if time_proportion < 0.5 :
+        mlm_loss , attentions_st, values_st, prediction_score_st = \
+            student.forward(batch, output_attentions=True, output_values=True, output_loss=True)
+        if master_process(args):
+            wandb.log({"train/loss": mlm_loss}, step=global_step)
+        total_loss = mlm_loss
+    else:
+        with torch.no_grad():
+            attentions_teacher, values_teacher, prediction_score_teacher = \
+                teacher(batch, output_attentions=True, output_values=True, output_loss=False)
+        mlm_loss , attentions_st, values_st, prediction_score_st = \
+            student.forward(batch, output_attentions=True, output_values=True, output_loss=True)
+
+        loss_att, loss_val = \
+                att_val_kl(attentions_st, values_st, attentions_teacher, values_teacher, args.layer_selection)
+        total_loss = loss_att + loss_val
+        if master_process(args):
+            wandb.log({"train/loss": total_loss}, step=global_step)
+            wandb.log({"train/loss_att": loss_att}, step=global_step)
+            wandb.log({"train/loss_val": loss_val}, step=global_step)
+    return total_loss 
